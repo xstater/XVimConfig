009a4f62008d8e12cb0e73abc311ec24ade105a0
xstater@clevo <a166224@163.com>
2023-05-18 01:56:52 +0800

    fixed visual mode which key

--- lua/vscode/keymaps.lua
+++ lua/vscode/keymaps.lua
@@ -1,2 +1,3 @@
 -- trigger "Whcih Key" Plugin in VSCode
-vim.keymap.set({ "n" ,"v" },"<space>", ":call VSCodeNotify('whichkey.show')<CR>", { noremap = true})
+vim.keymap.set('n','<space>', "<Cmd>call VSCodeNotify('whichkey.show')<CR>", { noremap = true })
+vim.keymap.set({ 'v', 'x' },'<space>', "<Cmd>call VSCodeNotifyVisual('whichkey.show', 0)<CR>", { noremap = true })
