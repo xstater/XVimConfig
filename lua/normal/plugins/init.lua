777861e75dfb7d797be3094bcbfe35658a84d506
xstater <a166224@163.com>
2026-08-01 02:25:37 +0800

    增加F# lsp (暂未生效)

--- lua/normal/plugins/init.lua
+++ lua/normal/plugins/init.lua
@@ -103,4 +103,19 @@ require 'lazy'.setup({
             require 'normal.plugins.cmp_lsp'
         end
     },
+    -- F# support
+    'ionide/Ionide-vim',
+    ft = { 'fsharp', 'fsharp_project' },
+    config = function()
+        -- if you need extra configuration/events, example with 'Show tooltips on CursorHold'
+        -- vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+        --   pattern = { '*.fs', '*.fsi', '*.fsx' },
+        --   callback = function()
+        --     vim.fn['fsharp#showTooltip']()
+        --  end,
+        -- })
+        
+        -- Show fsi in horizontal split
+        -- vim.g["fsharp#fsi_window_command"] = "vnew"
+    end
 })
